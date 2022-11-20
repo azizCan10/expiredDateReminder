2d3b780f5f2d30a40da7e57b19b83d64934b42b4
Çağrı Şentürk <csenturk11@hotmail.com>
2022-11-20 22:31:17 +0300

    Main activity update

--- app/src/main/java/com/example/expireddatereminder/MainActivity.java
+++ app/src/main/java/com/example/expireddatereminder/MainActivity.java
@@ -11,7 +11,11 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 
+import com.example.expireddatereminder.business.abstracts.ProductService;
+import com.example.expireddatereminder.business.concretes.ProductManager;
+import com.example.expireddatereminder.dataAccess.concretes.ProductRepositoryImpl;
 import com.example.expireddatereminder.databinding.ActivityMainBinding;
+import com.example.expireddatereminder.entities.concretes.Product;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,5 +33,22 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(binding.toolbar.toolbar);
 
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        binding.fabAddProduct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AddProductActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        productAdapter =new RecyclerViewProductAdapter(this);
+        binding.recyclerView.setAdapter(productAdapter);
+        binding.recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
